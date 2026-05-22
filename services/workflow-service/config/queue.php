74f19edfad69c9fb2b47c29c46c0bbf79ee66163
Muchlis Adhi <adi.wiratama@gmail.com>
2026-05-22 10:31:44 +0700

    fix: add rabbitmq connection config to queue.php for execution and workflow services

--- services/workflow-service/config/queue.php
+++ services/workflow-service/config/queue.php
@@ -10,6 +10,24 @@ return [
             'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => 90,
         ],
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+            'port' => env('RABBITMQ_PORT', 5672),
+            'user' => env('RABBITMQ_USER', 'guest'),
+            'password' => env('RABBITMQ_PASSWORD', 'guest'),
+            'vhost' => env('RABBITMQ_VHOST', '/'),
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+            'options' => [
+                'queue' => [
+                    'exchange' => env('RABBITMQ_EXCHANGE', ''),
+                    'exchange_type' => env('RABBITMQ_EXCHANGE_TYPE', 'direct'),
+                    'exchange_routing_key' => '',
+                    'prioritize_delayed' => false,
+                    'queue_max_priority' => 10,
+                ],
+            ],
+        ],
     ],
     'failed' => ['driver' => 'database-uuids', 'database' => env('DB_CONNECTION', 'pgsql'), 'table' => 'failed_jobs'],
 ];
